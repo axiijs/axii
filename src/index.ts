3883e9cefc66401269d911ad27219a8a488ad969
sskyy <skyking_H@hotmail.com>
2024-02-21 20:21:16 +0800

    fix: type Root

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export { createElement, Fragment, createElementNS, dispatchEvent } from './DOM'
 export { configure } from './ComponentHost'
 export { default as propTypes } from './propTypes'
-export { createRoot, Root } from './render'
+export { createRoot } from './render'
 export { createHost } from './createHost'
 export * from './eventAlias'
 export * from './Host'
