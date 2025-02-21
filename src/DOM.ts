a341882863b88973baf18cd707803ada952d1862
sskyy <skyking_H@hotmail.com>
2025-02-21 22:48:26 +0800

    fix: borderWidth not border with auto unit

--- src/DOM.ts
+++ src/DOM.ts
@@ -2,7 +2,7 @@
 import {assert, each, isPlainObject} from './util'
 import {Component, ComponentNode} from "./types";
 
-export const AUTO_ADD_UNIT_ATTR = /^(width|height|top|left|right|bottom|margin|padding|border|fontSize|maxWidth|maxHeight|minHeight|minWidth|gap|flexBasis|columnGap|rowGap)/
+export const AUTO_ADD_UNIT_ATTR = /^(width|height|top|left|right|bottom|margin|padding|borderWidth|fontSize|maxWidth|maxHeight|minHeight|minWidth|gap|flexBasis|columnGap|rowGap)$/
 let autoUnitType: 'px' | 'rem' | 'em' = 'px'
 export function setAutoUnitType(type: 'px' | 'rem' | 'em') {
     autoUnitType = type
